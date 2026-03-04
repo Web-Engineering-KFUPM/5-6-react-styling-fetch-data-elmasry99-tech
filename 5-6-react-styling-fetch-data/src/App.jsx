41e0fe20353fe08a8ff79289494b4d045682c653
John Doe <johndoe@email.com>
2026-03-04 08:42:08 +0300

    Done 3

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -234,10 +234,12 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.2: Implement filtering users here (see lab instructions)
-    if(searchTerm.trim() === ""){
+    if (!searchTerm) {
       setFilteredUsers(users);
-    }else{
-      const filtered = users.filter((user)=>user.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    } else {
+      const filtered = users.filter((user) =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
       setFilteredUsers(filtered);
     }
   }, [searchTerm, users]);
